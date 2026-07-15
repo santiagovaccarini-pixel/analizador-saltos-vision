02f320738bea21babe1aa791e9833189f2224896
santiagovaccarini-pixel <santiago.vaccarini@istea.com.ar>
2026-07-15 16:47:18 -0300

    Completar constantes anatómicas

--- src/constants.py
+++ src/constants.py
@@ -1,18 +1,49 @@
-"""Constantes anatómicas y de configuración usadas por el proyecto."""
+"""Constantes anatómicas y de configuración del proyecto."""
+
+from __future__ import annotations
 
 LANDMARK_NAMES = {
+    0: "nose",
     11: "left_shoulder",
     12: "right_shoulder",
+    13: "left_elbow",
+    14: "right_elbow",
+    15: "left_wrist",
+    16: "right_wrist",
     23: "left_hip",
     24: "right_hip",
     25: "left_knee",
     26: "right_knee",
     27: "left_ankle",
     28: "right_ankle",
+    29: "left_heel",
+    30: "right_heel",
     31: "left_foot_index",
     32: "right_foot_index",
 }
 
-VIDEO_EXTENSIONS = {".mp4", ".mov", ".avi", ".mkv"}
-MIN_VISIBILITY = 0.50
-DEFAULT_FPS = 30.0
+LOWER_BODY_INDICES = (23, 24, 25, 26, 27, 28, 29, 30, 31, 32)
+
+POSE_CONNECTIONS = (
+    (11, 12),
+    (11, 13),
+    (13, 15),
+    (12, 14),
+    (14, 16),
+    (11, 23),
+    (12, 24),
+    (23, 24),
+    (23, 25),
+    (25, 27),
+    (27, 29),
+    (29, 31),
+    (27, 31),
+    (24, 26),
+    (26, 28),
+    (28, 30),
+    (30, 32),
+    (28, 32),
+)
+
+SUPPORTED_VIDEO_EXTENSIONS = {".mp4", ".mov", ".avi", ".mkv", ".m4v"}
+MIN_VISIBILITY = 0.45
